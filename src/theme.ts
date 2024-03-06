f95609d08dec511be175f5a121777758c1d8a3d1
Alex Nascimento <82345139+alexdev613@users.noreply.github.com>
2024-03-06 17:36:36 -0300

    Add files via upload

---
+++ src/theme.ts
@@ -0,0 +1,36 @@
+import { createTheme, responsiveFontSizes } from "@mui/material";
+
+let theme = createTheme({
+    palette: {
+        primary: {
+            main: '#232323',
+        },
+        secondary: {
+            main: '#4f8e3e',
+        },
+    },
+    typography: {
+        fontFamily: [
+            // "Arial",
+            // "Segoe Print",
+            "Yu Gothic",
+            // "Script Regular",
+            // "Lucida Sans",
+            // '"Helvetica Neue"',
+            // '-apple-system',
+            // 'BlinkMacSystemFont',
+            // '"Segoe UI"',
+            // 'Roboto',
+            // '"Helvetica Neue"',
+            // 'Arial',
+            // 'sans-serif',
+            // '"Apple Color Emoji"',
+            // '"Segoe UI Emoji"',
+            // '"Segoe UI Symbol"',
+        ].join(','),
+    }
+});
+
+theme = responsiveFontSizes(theme);
+
+export default theme;
